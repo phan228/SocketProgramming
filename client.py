e825c1b0e82aecdfa3c8dd92cac78ff5f6b23086
Ngoc Phan <ngocph228@gmail.com>
2020-12-10 20:35:37 -0500

    Add client and server code

---
+++ client.py
@@ -0,0 +1,138 @@
+import socket
+import os
+import time
+
+SEPARATOR = "<SEPARATOR>"
+pathClient = '/Users/thttien/documents/1_Susu/spring2020/cs371/Client/'
+pathServer = '/Users/thttien/documents/1_Susu/spring2020/cs371/Server/'
+pathInfo = '/Users/thttien/documents/1_Susu/spring2020/cs371/Info/'
+
+def Main():
+	# Create the socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Connect
+	host = socket.gethostname()
+
+	# Send/ receive
+	print("Enter \"connect\", \"download\", \"upload\", \"delete\", \"dir\" (or \"q\" to quit)")
+	action = raw_input("-> ")
+	
+	if action == "connect":
+			portNum = raw_input("Port: ")
+			port = int(portNum)
+			s.connect((host, port))
+			action = raw_input("-> ")
+
+	if action != 'q':
+		port = 5000
+		s.connect((host, port))	
+
+		s.send(action)
+		if action == 'download':
+			filename = raw_input("Filename: ")
+			s.send(filename)
+			data = s.recv(1024)
+			if data[:5] == 'FOUND':
+				filesize = long(data[5:])
+				response = raw_input("File exists, download? (y/n): ")
+				s.send(response)
+				# Save the file after download
+				if response == 'y':
+					start_time = time.time()
+					# new file
+					downloaded = open(pathClient + 'downloaded_' + filename, 'wb')	
+					# Receive downloaded data and write it in the new file
+					data = s.recv(1024)
+					downloaded.write(data)
+					received = len(data)
+					elasped_time = time.time() - start_time
+					if elasped_time > 0:
+						download_speed = received/(1000000 * elasped_time)
+						download_speed = str(round(download_speed, 2))
+					else:
+						download_speed = 'TOO FAST'
+						print(download_speed)
+					# Make sure to get all data
+					count = 0
+					while received < filesize:
+						data = s.recv(1024)
+						received += len(data)
+						downloaded.write(data)
+						elasped_time = time.time() - start_time
+						count += 1
+						if elasped_time > 0:
+							download_speed = received/(1000000 * elasped_time)
+							download_speed = str(round(download_speed, 2))
+							if (count % 12000) == 0:
+								print(download_speed)
+						else:
+							download_speed = 'TOO FAST'
+							print(download_speed)
+					# Done downloading
+					downloaded.close()
+					elasped_time = time.time() - start_time
+					if elasped_time > 0:
+						avg_download_speed = filesize/(1000000 * elasped_time)
+						avg_download_speed = str(round(avg_download_speed, 2))
+					else:
+						avg_download_speed = 'FAST'
+
+					print("Average download speed = " + avg_download_speed + " mbps")
+
+					info = open(pathInfo + 'downloadRecord.txt', 'a+')
+					if os.stat(pathInfo + "downloadRecord.txt").st_size == 0:
+						info.write(filename + " " + "1 \n")
+					else:
+						exist = False
+						for line in info:
+							print(line)
+							read = line.split()
+							if read[0] == filename:
+								exist = True
+								newNum = int(read[1]) 
+								newNum += 1
+								line.replace(read[1], str(newNum))
+								print(read[1])
+								break
+						
+						if exist == False:
+							numDownload = 1
+							info.write(filename + " " + str(numDownload) + "\n")
+					
+					info.close()
+					print("Download completed!")
+			else:
+				print("Error!")
+
+		if action == 'upload':
+			filename = raw_input("Filename: ")
+			os.chdir(pathClient)
+			if os.path.isfile(filename):
+				filesize = os.path.getsize(filename)
+				s.send(filename)
+				s.send(str(filesize))			
+				with open(filename, 'rb') as f:
+					data = f.read(1024)
+					s.send(data)
+					# In case the file is more than 1024 bytes
+					while data != "":
+						data = f.read(1024)
+						s.send(data)
+					f.close()
+			else:
+				print("Error!")
+
+		if action == 'delete':
+			filename = raw_input("Filename: ")
+			s.send(filename)
+
+		if action == 'dir':
+			foldername = raw_input("Folder name: ")
+			s.send(foldername)
+
+	# Close the connection
+	s.close()
+
+if __name__ == '__main__':
+	Main()
---
+++ server.py
@@ -0,0 +1,164 @@
+import socket
+import os
+import threading
+import datetime
+import time
+
+SEPARATOR = "<SEPARATOR>"
+pathClient = '/Users/thttien/documents/1_Susu/spring2020/cs371/Client/'
+pathServer = '/Users/thttien/documents/1_Susu/spring2020/cs371/Server/'
+pathInfo = '/Users/thttien/documents/1_Susu/spring2020/cs371/Info/'
+path371 = '/Users/thttien/documents/1_Susu/spring2020/cs371/'
+
+def download(file, socket):
+	filename = socket.recv(1024)
+
+	# if the file we want to download exists
+	os.chdir(pathServer)
+	if os.path.isfile(filename):
+		socket.send("FOUND" + str(os.path.getsize(filename)))
+		clientRes = socket.recv(1024)
+		if clientRes[:1] == 'y':
+			with open(filename, 'rb') as f:
+				data = f.read(1024)
+				socket.send(data)
+				# In case the file is more than 1024 bytes
+				while data != "":
+					data = f.read(1024)
+					socket.send(data)
+		else:
+			print("Download cancelled")
+
+	# if the file is not there
+	else:
+		socket.send("ERROR")
+
+def Main():
+	# Create the socket
+	# AF_INET == ipv4
+	# SOCK_STREAM == TCP
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Binding
+	host = socket.gethostname()
+	port = 5000
+	s.bind((host, port))
+
+	# Listen
+	s.listen(5)
+
+	while True:
+		# Accept
+		connection, address = s.accept()
+		print("Connection from : " + str(address))
+
+		action = connection.recv(1024)
+
+		# Send/ receive
+		if action ==  "download":
+			t = threading.Thread(target=download, args=("retrThread", connection))
+			t.start()
+
+		if action == "upload":
+			filename = connection.recv(1024)
+			size = connection.recv(1024)
+			filesize = int(size)
+			start_time = time.time()
+			print("Uploading...")
+			with open(pathServer + 'uploaded_' + filename, "wb") as uploaded:
+				bytes_read = connection.recv(1024)
+			
+				# write to the file the bytes we just received
+				uploaded.write(bytes_read)
+				received = len(bytes_read)
+				elasped_time = time.time() - start_time
+				if elasped_time > 0:
+					upload_speed = received/(1000000 * elasped_time)
+					upload_speed = str(round(upload_speed, 2))
+				else:
+					upload_speed = 'TOO FAST'
+					print(upload_speed)
+				# Make sure to get all data
+				count = 0
+				while received < filesize:
+					go  = time.time()
+					data = connection.recv(1024)
+					uploaded.write(data)
+					received += len(data)
+					elasped_time = time.time() - start_time
+					count += 1
+					if elasped_time > 0:
+						upload_speed = received/(1000000 * elasped_time)
+						upload_speed = str(round(upload_speed, 2))
+						if (count % 10000) == 0:
+							print(upload_speed)
+					else:
+						upload_speed = 'TOO FAST'
+						print(upload_speed)			    
+			# Done downloading
+			uploaded.close()
+			elasped_time = time.time() - start_time
+			if elasped_time > 0:
+				avg_upload_speed = filesize/(1000000 * elasped_time)
+				avg_upload_speed = str(round(avg_upload_speed, 2))
+			else:
+				avg_upload_speed = 'FAST'
+
+			print("Average upload speed = " + avg_upload_speed + " mbps")
+			
+			now = datetime.datetime.now()
+			info = open(pathInfo + "info.txt", 'a')
+			info.write(filename + " " + str(filesize) + " bytes " + str(now) + '\n')
+			info.close()
+
+			print("Upload completed!")
+
+		if action  == "delete":
+			filename = connection.recv(1024)
+			os.chdir(pathServer)
+			if os.path.isfile(filename):
+				os.remove(filename)
+				print(filename + " removed!")
+			else:
+				os.chdir(pathClient)
+				if os.path.isfile(filename):
+					os.remove(filename)
+					print(filename + " removed!")
+				else:
+					print(filename + " does not exists!")
+			os.chdir(pathServer)
+
+		if action == "dir":
+			os.chdir(path371)
+			foldername = connection.recv(1024)
+			if os.path.isdir(foldername):
+				files = os.listdir(foldername)
+				for file in files:
+					here = False
+					info = open(pathInfo + "info.txt", 'r')  
+					dload = open(pathInfo + "downloadRecord.txt", 'r')
+					for x in info:
+						read = x.split()
+						if read[0] == file:
+							here = True
+							print(x.strip())
+					
+					for i in dload:
+						read = i.split()
+						if (read[0] == file):
+							here = True
+							print(i.strip())
+					if here == False:
+						print(file)
+					info.close()
+					dload.close()
+			else:
+				print("Folder does not exsist!")		
+
+
+	# Close the connection
+	connection.close()
+	s.close()
+
+if __name__ == '__main__':
+	Main()
